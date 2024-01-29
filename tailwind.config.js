4f670c2c717a87f9f9da8d574089fbb58bf34e06
abbey eleazer <abbeyelazer@gmail.com>
2024-01-29 16:30:55 +0200

    gams & lams landing page

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+   
+    extend: {
+      colors:{
+        colorPrimary: '#070D59',
+        colorSecondary: '#1F3C88',
+        colorTertiary: '#5893D4',
+        accentColor: '#CEDDEF'
+       },
+    },
+  },
+  plugins: [],
+}
+
